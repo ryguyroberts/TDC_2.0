fd5989aa42bbe2b26fc6eb181f23614482f8ceb9
Ryan Robertson <ryguyrobertson@gmail.com>
2024-04-16 09:34:34 -0700

    move template scenes, remove template asset

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
-import { Boot } from './scenes/Boot';
-import { Game as MainGame } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
-import { MainMenu } from './scenes/MainMenu';
-import { Preloader } from './scenes/Preloader';
-
 import { Game, Types } from "phaser";
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+
 const config: Types.Core.GameConfig = {
     type: Phaser.AUTO,
     width: 1024,
@@ -18,13 +13,7 @@ const config: Types.Core.GameConfig = {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+    scene: []
 };
 
 export default new Game(config);
